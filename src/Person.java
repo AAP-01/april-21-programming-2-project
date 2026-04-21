f555b9ceded7841cacf6f773a3aa7d0e0230ca1d
AAP-01 <jbthememer43@gmail.com>
2026-04-21 11:29:46 -0400

    1 step completed

---
+++ src/Person.java
@@ -0,0 +1,93 @@
+import java.time.LocalDate;
+
+public abstract class Person {
+
+    protected int employeeID;
+    protected String firstName;
+    protected String lastName;
+    protected int age;
+    protected LocalDate hireDate;
+    protected String address;
+    protected String phoneNumber;
+
+    public Person(){
+
+    }
+
+    public Person(int employeeID, String firstName, String lastName, int age, LocalDate hireDate, String address, String phoneNumber){
+        this.employeeID = employeeID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.hireDate = hireDate;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public abstract void displayInfo();
+
+    public int getEmployeeID(){
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID){
+        this.employeeID = employeeID;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public LocalDate getHireDate(){
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate){
+        this.hireDate = hireDate;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString(){
+        return "Person{" + "employeeID = " + employeeID + ", firstName = " + firstName + ", lastName = " + lastName + ", age = " + age + ", hireDate = " + hireDate + ", address = " + address + ", phoneNumber = " + phoneNumber + " }";
+    }
+
+
+
+
+}
